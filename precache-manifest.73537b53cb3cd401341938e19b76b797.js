1e381c23f59ee17a9cb78d4b5e5d7b8a4899994d
Eduardo Urbaez <eurbaezjr@gmail.com>
2020-05-19 00:48:32 -0700

    Updates

---
+++ precache-manifest.73537b53cb3cd401341938e19b76b797.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d940effdf4cbe057484ab37246e22452",
+    "url": "/about/index.html"
+  },
+  {
+    "revision": "ce81db3f537aee0c9f6e",
+    "url": "/about/static/css/main.3b091522.chunk.css"
+  },
+  {
+    "revision": "3ae240f7ac86a0edb957",
+    "url": "/about/static/js/2.4bee887a.chunk.js"
+  },
+  {
+    "revision": "fa24d2988fd9ee29f9cba16979460922",
+    "url": "/about/static/js/2.4bee887a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce81db3f537aee0c9f6e",
+    "url": "/about/static/js/main.05bbf17d.chunk.js"
+  },
+  {
+    "revision": "7f53a6c9b438a4c9acde",
+    "url": "/about/static/js/runtime-main.add558fd.js"
+  }
+]);
